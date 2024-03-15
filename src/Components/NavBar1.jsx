86b4fe05d6d4195441b8e46726f05d3650cad867
tanjummotin <tanjummotin@gmail.com>
2024-03-15 14:33:50 +0600

    showing username after login

--- src/Components/NavBar1.jsx
+++ src/Components/NavBar1.jsx
@@ -4,23 +4,27 @@ import { Link } from 'react-router-dom';
 const CUETLogo = require('../assets/CUETALUMNI_finall.png');
 
 export default function NavBar1() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userName, setUserName] = useState('');
+ 
+  const userData = JSON.parse(sessionStorage.getItem("userData"));
+  const username = userData?.username;
+  const loggedIn = sessionStorage.getItem("loggedIn");
 
-  useEffect(() => {
-    // You should replace this placeholder with an actual API call to check login status
-    // Fetch the data from your server using an API endpoint
-    // Example: /api/getLoginStatus
-    fetch("http://localhost/test/login_status.php")
-      .then((response) => response.json())
-      .then((data) => {
-        setIsLoggedIn(data.isLogged);
-        setUserName(data.userName);
-      })
-      .catch((error) => {
-        console.error('Error fetching login status:', error);
-      });
-  }, []);
+
+
+  // useEffect(() => {
+  //   // You should replace this placeholder with an actual API call to check login status
+  //   // Fetch the data from your server using an API endpoint
+  //   // Example: /api/getLoginStatus
+  //   fetch("http://localhost/test/login_status.php")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setIsLoggedIn(data.isLogged);
+  //       setUserName(data.userName);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error fetching login status:', error);
+  //     });
+  // }, []);
 
   return (
     <nav className="nav2 navbar navbar-expand-lg">
@@ -34,10 +38,10 @@ export default function NavBar1() {
               Alumni Directory
             </Link>
           </li>
-          {isLoggedIn ? (
+          {loggedIn ? (
             <li className="nav-item">
               <Link className="nav-link ps-4 pe-4 login-btn" to="/dashboard">
-                Welcome, {userName}
+                Welcome, {username}
               </Link>
             </li>
           ) : (
