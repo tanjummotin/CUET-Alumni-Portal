8eafbbfeaa25e0773794266f7127f4d25f1841b0
tanjummotin <tanjummotin@gmail.com>
2024-03-16 19:09:26 +0600

    change in about page

--- src/Components/about.jsx
+++ src/Components/about.jsx
@@ -3,14 +3,14 @@ import React from "react";
 export default function About() {
   return (
     <div className="container">
-      <h1 className="text-center mt-5">Cuet Alumni</h1>
+      <h1 className="text-center mt-5">CUET ALUMNI</h1>
       <div className="row mt-5">
         <div className="col-md-4">
           <h3>WHO WE ARE</h3>
           <p>
             Our Mission <br />
             To create a purposeful and forward-looking organization for the
-            alumni of UST for their social, cultural, and professional pursuits;
+            alumni of CUET for their social, cultural, and professional pursuits;
             for fostering greater cohesion and fellowship among themselves for
             rendering all possible support to the transformation of the beloved
             alma mater into an institution of higher learning in technical and
@@ -31,29 +31,29 @@ export default function About() {
             </li>
             <li>
               Appropriately, align, and complement the activities of the various
-              societies or clubs of UST graduates that may exist in Bangladesh
+              societies or clubs of CUET graduates that may exist in Bangladesh
               and in foreign countries
             </li>
             <li>
-              Provide support to the members and their families as well as UST
+              Provide support to the members and their families as well as CUET
               students, staff members in distress, and to organize humanitarian
               activities in times of national calamities
             </li>
             <li>
-              In collaboration and consultation with UST authority, carry out
+              In collaboration and consultation with CUET authority, carry out
               the following activities:
             </li>
             <ul>
               <li>
-                Enhance, uphold and promote the image and heritage of UST as a
+                Enhance, uphold and promote the image and heritage of CUET as a
                 Center of Excellence in technical and science education and
                 research in Bangladesh and abroad
               </li>
               <li>
-                Support efforts of UST and other appropriate organizations for
+                Support efforts of CUET and other appropriate organizations for
                 enhancing the quality of education and research and for
                 establishing state-of-the-art educational and research
-                facilities at UST, including helping students and faculty
+                facilities at CUET, including helping students and faculty
                 members to participate in conferences, seminars, and
                 competitions
               </li>
@@ -61,16 +61,16 @@ export default function About() {
                 Raise funds from Members and Patrons of the Association as well
                 as from alumni, individuals, and organizations at home and
                 abroad for implementing development projects that have been
-                chosen jointly with UST authority for purposes indicated in (2)
+                chosen jointly with CUET authority for purposes indicated in (2)
                 above
               </li>
               <li>
-                Facilitate intellectual exchanges between UST and advanced
+                Facilitate intellectual exchanges between CUET and advanced
                 universities and Institutions in other countries
               </li>
               <li>
                 Offer career counseling to students, graduates, and others
-                associated with UST
+                associated with CUET
               </li>
             </ul>
           </ul>
